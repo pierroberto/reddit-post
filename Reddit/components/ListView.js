c51816ee958ecbf995c204ea58971f4a4baec368
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-12-19 19:15:27 +0100

    Feat: add refresh list

--- Reddit/components/ListView.js
+++ Reddit/components/ListView.js
@@ -8,13 +8,12 @@ import { StackNavigator } from "react-navigation";
 export default class ListView extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { posts: [] };
+    this.state = { posts: [], refreshing: false };
   }
   getPosts = async () => {
     const list = await fetch("https://api.reddit.com/r/pics/new.json");
     const rawData = JSON.parse(list._bodyInit).data.children;
     const postsInfo = rawData.map(post => {
-      console.log(post.data.permalink);
       return {
         id: post.data.id,
         author: post.data.author,
@@ -26,15 +25,19 @@ export default class ListView extends React.Component {
         link: `https://i.reddit.com${post.data.permalink}`
       };
     });
-    this.setState({ posts: postsInfo });
+    this.setState({ posts: postsInfo, refreshing: false });
   };
 
-  getInfo = async () => {
-    const postsInfo = await this.getPosts();
+  handleRefresh = async () => {
+    console.log("hello");
+    this.setState({
+      refreshing: true,
+      posts: []
+    });
   };
-
   // =========== RENDERING
   render() {
+    console.log("refreshing... ", this.state.posts.length);
     const { navigate } = this.props.navigation;
     if (!this.state.posts.length) this.getPosts();
     return (
@@ -46,6 +49,8 @@ export default class ListView extends React.Component {
               <ItemView postInfo={item} navigation={navigate} />
             )}
             keyExtractor={item => item.id}
+            refreshing={this.state.refreshing}
+            onRefresh={() => this.handleRefresh()}
           />
         </List>
       </View>
